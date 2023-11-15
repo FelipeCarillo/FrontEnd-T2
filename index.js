258f9e7fdb08b6c2954c0239da3321328140960b
Felipe Carillo <felipecarillo@outlook.com>
2023-11-15 17:16:43 -0300

    T2

---
+++ index.js
@@ -0,0 +1,47 @@
+const port = 3000;
+const cors = require('cors');
+const express = require('express');
+const API_KEY_OPENWEATHER = "ef0b0973b783e0614ac87612ec04344b";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/city', (req, res) => {
+    let city = req.body.city;
+    async function getCoordinates(city) {
+        let endpoint = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY_OPENWEATHER}`;
+        const response = await fetch(endpoint);
+        const data = await response.json();
+        return data[0];
+    }
+    getCoordinates(city)
+        .then(response => {
+            let lat = response.lat;
+            let lon = response.lon;
+            res.send({ lat, lon });
+            },
+        )
+    }
+);
+
+app.get('/weather', (req, res) => {
+    let lat = req.query.lat;
+    let lon = req.query.lon;
+    async function getWeather(lat, lon) {
+        let endpoint = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY_OPENWEATHER}`;
+        const response = await fetch(endpoint);
+        const data = await response.json();
+        return data;
+    }
+    getWeather(lat, lon).then(data => {
+        let feels_like = data.main.feels_like;
+        let description = data.weather[0].description;
+        res.send({ feels_like, description });
+    })
+});
+
+app.listen(port, () => {
+    console.log("Server is running on port: " + port);
+});
+
---
+++ src/js/index.js
@@ -0,0 +1,45 @@
+function getCoordinates() {
+    document.getElementById("submit").innerHTML = 'Buscando <i class="fa-solid fa-spinner fa-spin-pulse"></i>';
+    let city = document.getElementById("city").value;
+    let endpoint = "http://localhost:3000/city";
+    let data = { city };
+    fetch(endpoint, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(response => {
+        return response.json();
+    }).then(data => {
+        let lat = data.lat;
+        let lon = data.lon;
+        getWeather(lat, lon)
+    }).catch(error => {
+        console.log("Error" + error);
+    });
+}
+
+function getWeather(lat, lon) {
+    let endpoint = `http://localhost:3000/weather?lat=${lat}&lon=${lon}`;
+    fetch(endpoint).then(response => {
+        return response.json();
+    }).then(data => {
+        let feels_like = (data.feels_like - 273.15).toFixed(2) + "°C";
+        let description = data.description.charAt(0).toUpperCase() + data.description.slice(1);
+        document.getElementById("submit").innerHTML = "Buscar";
+        document.getElementById("lat").innerHTML = lat;
+        document.getElementById("lon").innerHTML = lon;
+        document.getElementById("feels_like").innerHTML = feels_like;
+        document.getElementById("description").innerHTML = description;
+        console.log("Latitude: " + lat + "\nLongitude: " + lon);
+        console.log("Feels like: " + feels_like + "\nDescription: " + description);
+    });
+}
+
+function clearResponses() {
+    document.getElementById("lat").innerHTML = "Latitude";
+    document.getElementById("lon").innerHTML = "Longitude";
+    document.getElementById("feels_like").innerHTML = '<i class="fa-solid fa-cloud fa-bounce" style="color: #727272;">°C</i>';
+    document.getElementById("description").innerHTML = "";
+}
